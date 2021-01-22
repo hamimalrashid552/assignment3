3f98f5476be9fc3594c1145c4ae4af1fde4a2ff7
Md. Hamim Al Rashid <hamimalrashid552@gmail.com>
2021-01-22 14:32:53 +0600

    second task done

--- assignment.js
+++ assignment.js
@@ -1,3 +1,6 @@
+
+kilometerToMeter
+
 function kilometerToMeter(km) {
     var meter = 1000;
     if (km < 0) {
@@ -7,5 +10,22 @@ function kilometerToMeter(km) {
         return km * meter;
     }
 }
-var result = kilometerToMeter(7);
-console.log(result);
+
+console.log(kilometerToMeter(7));
+
+budgetCalculator
+
+function budgetCalculator(watch, phone, laptop) {
+    if (watch < 0 || phone < 0 || laptop < 0) {
+        return "Invalid item provided";
+    }
+    else {
+        var totalWatchPrice = watch * 50;
+        var totalPhonrPrice = phone * 100;
+        var totalLaptopPrice = laptop * 500;
+
+        var total = totalWatchPrice + totalPhonrPrice + totalLaptopPrice;
+        return total;
+    }
+}
+console.log(budgetCalculator(10, 5, 1));
